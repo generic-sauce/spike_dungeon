2359b44e3c1bb51665e3206ceceb1aee57610f19
memoryleak47 <memoryleak47@protonmail.ch>
2019-12-28 22:58:10 +0100

    added tilemap

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,15 +1,19 @@
 mod player;
+mod tilemap;
 
 use player::Player;
+use tilemap::TileMap;
 
 pub struct World {
-	players: [Player; 2]
+	players: [Player; 2],
+	tilemap: TileMap,
 }
 
 impl World {
 	pub fn new() -> World {
 		World {
-			players: [Player::new(), Player::new()]
+			players: [Player::new(), Player::new()],
+			tilemap: TileMap::new(),
 		}
 	}
 }
