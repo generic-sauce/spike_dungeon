35a4111eaf5f6c5acbf439e3f07a9252e0e69938
memoryleak47 <memoryleak47@protonmail.ch>
2019-12-29 16:00:21 +0100

    built init_vec macro

--- src/vec/mod.rs
+++ src/vec/mod.rs
@@ -1,4 +1,7 @@
 use sfml::system::Vector2f;
+use std::cmp::{max, min};
+use std::ops::{Add, Sub, Mul, Div};
+use std::fmt::{Display, Debug, Error, Formatter};
 
 pub const TILESIZE: i32 = 32;
 
@@ -31,3 +34,141 @@ impl From<WorldVec> for Vector2f {
 		Vector2f::new(v.x as f32, v.y as f32)
 	}
 }
+
+macro_rules! init_vec {
+	($name:ident) => {
+		impl $name {
+			pub const fn new(x: i32, y: i32) -> $name {
+				$name { x, y }
+			}
+
+			pub const fn with(xy: i32) -> $name {
+				$name { x: xy, y: xy }
+			}
+
+			pub fn max(self, v: $name) -> $name {
+				$name { x: max(v.x, self.x), y: max(v.y, self.y) }
+			}
+
+			pub fn min(self, v: $name) -> $name {
+				$name { x: min(v.x, self.x), y: min(v.y, self.y) }
+			}
+
+			pub fn length(self) -> i32 {
+				((self.x * self.x + self.y * self.y) as f32).sqrt() as i32
+			}
+		}
+
+		impl Default for $name {
+			fn default() -> Self {
+				Self { x: 0, y: 0, }
+			}
+		}
+
+		impl Add<$name> for $name {
+			type Output = $name;
+
+			fn add(self, other: $name) -> $name {
+				$name::new (
+					self.x + other.x,
+					self.y + other.y,
+				)
+			}
+		}
+
+		impl Add<i32> for $name {
+			type Output = $name;
+
+			fn add(self, other: i32) -> $name {
+				$name::new (
+					self.x + other,
+					self.y + other,
+				)
+			}
+		}
+
+		impl Sub<$name> for $name {
+			type Output = $name;
+
+			fn sub(self, other: $name) -> $name {
+				$name::new (
+					self.x - other.x,
+					self.y - other.y,
+				)
+			}
+		}
+
+		impl Sub<i32> for $name {
+			type Output = $name;
+
+			fn sub(self, other: i32) -> $name {
+				$name::new (
+					self.x - other,
+					self.y - other,
+				)
+			}
+		}
+
+		impl Mul<$name> for $name {
+			type Output = $name;
+
+			fn mul(self, other: $name) -> $name {
+				$name::new (
+					self.x * other.x,
+					self.y * other.y,
+				)
+			}
+		}
+
+		impl Mul<i32> for $name {
+			type Output = $name;
+
+			fn mul(self, other: i32) -> $name {
+				$name::new (
+					self.x * other,
+					self.y * other,
+				)
+			}
+		}
+
+		impl Div<$name> for $name {
+			type Output = $name;
+
+			fn div(self, other: $name) -> $name {
+				$name::new (
+					self.x / other.x,
+					self.y / other.y,
+				)
+			}
+		}
+
+		impl Div<i32> for $name {
+			type Output = $name;
+
+			fn div(self, other: i32) -> $name {
+				$name::new (
+					self.x / other,
+					self.y / other,
+				)
+			}
+		}
+
+		impl Display for $name {
+			fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+				let s = format!("$name({}, {})", self.x, self.y);
+				fmt.write_str(&*s)
+			}
+		}
+
+		impl Debug for $name {
+			fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+				let s = format!("$name({:?}, {:?})", self.x, self.y);
+				fmt.write_str(&*s)
+			}
+		}
+
+	}
+}
+
+init_vec!(WorldVec);
+init_vec!(TileVec);
--- src/vec/tilevec.rs
+++ src/vec/tilevec.rs
@@ -1,5 +1,3 @@
-use std::ops::{Add, Sub, Mul, Div};
-use std::fmt::{Display, Debug, Error, Formatter};
 use std::cmp::{min, max};
 
 use crate::world::TILEMAP_SIZE;
@@ -11,22 +9,6 @@ pub struct TileVec {
 }
 
 impl TileVec {
-	pub const fn new(x: i32, y: i32) -> TileVec {
-		TileVec { x, y }
-	}
-
-	pub const fn with(xy: i32) -> TileVec {
-		TileVec { x: xy, y: xy }
-	}
-
-	pub fn max(self, v: TileVec) -> TileVec {
-		TileVec { x: max(v.x, self.x), y: max(v.y, self.y) }
-	}
-
-	pub fn min(self, v: TileVec) -> TileVec {
-		TileVec { x: min(v.x, self.x), y: min(v.y, self.y) }
-	}
-
 	pub fn iter_all() -> impl Iterator<Item=TileVec> {
 		iproduct!(0..TILEMAP_SIZE.x, 0..TILEMAP_SIZE.y)
 			.map(|(x, y)| TileVec::new(x, y))
@@ -41,113 +23,5 @@ impl TileVec {
 
 		iproduct!(xmin..xmax, ymin..ymax)
 			.map(|(x, y)| TileVec::new(x, y))
-	}
-}
-
-impl Default for TileVec {
-	fn default() -> Self {
-		Self { x: 0, y: 0, }
-	}
-}
-
-impl Add<TileVec> for TileVec {
-	type Output = TileVec;
-
-	fn add(self, other: TileVec) -> TileVec {
-		TileVec::new (
-			self.x + other.x,
-			self.y + other.y,
-		)
-	}
-}
-
-impl Add<i32> for TileVec {
-	type Output = TileVec;
-
-	fn add(self, other: i32) -> TileVec {
-		TileVec::new (
-			self.x + other,
-			self.y + other,
-		)
-	}
-}
-
-impl Sub<TileVec> for TileVec {
-	type Output = TileVec;
-
-	fn sub(self, other: TileVec) -> TileVec {
-		TileVec::new (
-			self.x - other.x,
-			self.y - other.y,
-		)
-	}
-}
-
-impl Sub<i32> for TileVec {
-	type Output = TileVec;
-
-	fn sub(self, other: i32) -> TileVec {
-		TileVec::new (
-			self.x - other,
-			self.y - other,
-		)
-	}
-}
-
-impl Mul<TileVec> for TileVec {
-	type Output = TileVec;
-
-	fn mul(self, other: TileVec) -> TileVec {
-		TileVec::new (
-			self.x * other.x,
-			self.y * other.y,
-		)
-	}
-}
-
-impl Mul<i32> for TileVec {
-	type Output = TileVec;
-
-	fn mul(self, other: i32) -> TileVec {
-		TileVec::new (
-			self.x * other,
-			self.y * other,
-		)
-	}
-}
-
-impl Div<TileVec> for TileVec {
-	type Output = TileVec;
-
-	fn div(self, other: TileVec) -> TileVec {
-		TileVec::new (
-			self.x / other.x,
-			self.y / other.y,
-		)
-	}
-}
-
-impl Div<i32> for TileVec {
-	type Output = TileVec;
-
-	fn div(self, other: i32) -> TileVec {
-		TileVec::new (
-			self.x / other,
-			self.y / other,
-		)
-	}
-}
-
-impl Display for TileVec {
-	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-		let s = format!("TileVec({}, {})", self.x, self.y);
-		fmt.write_str(&*s)
-	}
-}
-
-impl Debug for TileVec {
-	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-		let s = format!("TileVec({:?}, {:?})", self.x, self.y);
-		fmt.write_str(&*s)
 	}
 }
